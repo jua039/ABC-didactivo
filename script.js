4075d52f7a03bfd7ce26982aac3f321c0f005a6b
KarenMartinez <k.nathalia.martinez.b@gmail.com>
2026-06-09 15:21:11 -0500

    organizando

--- script.js
+++ script.js
@@ -33,3 +33,19 @@ document.querySelectorAll(".filtro").forEach(boton => {
     });
 });
 //------------------------------------------
+
+let contador = 0;
+let AContador = document.getElementById("contador");
+
+
+function voltear(tarjeta) {
+    if(!tarjeta.classList.contains('cara-trasera')){
+        tarjeta.classList.add('cara-trasera');
+        contador = contador +1;
+        AContador.textContent = contador;
+    }else{
+        tarjeta.classList.remove('cara-trasera');
+        contador = contador-1;
+        AContador.textContent = contador;
+    }
+}
